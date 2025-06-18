1d1eaf95a1f5580ec9a3622bb19422c2482a55f2
Dilli Basker <dillibasker1@gmail.com>
2025-06-18 23:49:06 +0530

    l

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import matplotlib.pyplot as plt
 
 df=pd.read_csv('student_data.csv')
 x=df.drop('Pass',axis=1)
+y=df['Pass']
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+model=LogisticRegression()
+model.fit(x_train,y_train)
+
+predictions=model.predict(x_test)
+
+accuracy=accuracy_score(y_test,predictions)
+cm = confusion_matrix(y_test, predictions)
+
+print("✅ Model Accuracy:", round(accuracy * 100, 2), "%")
+# 7. Confusion Matrix Heatmap
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
